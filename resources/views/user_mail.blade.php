9e4bd04227a8e202c4de3d5d64e991501b68515b
vilsoni <vilsonisaku93@gmail.com>
2022-10-05 00:01:24 +0200

    users first 3 posts

--- resources/views/user_mail.blade.php
+++ resources/views/user_mail.blade.php
@@ -2,7 +2,7 @@
 @foreach($users as $user)
 
 {{ $user->name }} has written:
-@foreach($user->posts()->limit(3)->get() as $post)
+@foreach($user->posts()->orderBy('id')->limit(3)->get() as $post)
 -  {{ $post->title }}
 @endforeach
 
